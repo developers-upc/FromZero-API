97353e18bd2209eb0e1e75ca16865d6380f7cf8e
JorgeQuilla <u20211b197@upc.edu.pe>
2024-09-02 12:03:35 -0500

    fix(cors configuration): PATCH method added
    agregando metodo PATCH a configuracion CORS

--- src/main/java/com/acme/fromzeroapi/message/interfaces/rest/MessageController.java
+++ src/main/java/com/acme/fromzeroapi/message/interfaces/rest/MessageController.java
@@ -1,5 +1,6 @@
 package com.acme.fromzeroapi.message.interfaces.rest;
 
+import com.acme.fromzeroapi.message.application.internal.outboundservices.ExternalIamMessageService;
 import com.acme.fromzeroapi.message.domain.model.queries.GetAllMessageByRecipientByIdQuery;
 import com.acme.fromzeroapi.message.domain.services.MessageCommandService;
 import com.acme.fromzeroapi.message.domain.services.MessageQueryService;
@@ -7,7 +8,6 @@ import com.acme.fromzeroapi.message.interfaces.rest.resources.CreateMessageResou
 import com.acme.fromzeroapi.message.interfaces.rest.resources.MessageResource;
 import com.acme.fromzeroapi.message.interfaces.rest.transform.CreateMessageCommandFromResourceAssembler;
 import com.acme.fromzeroapi.message.interfaces.rest.transform.MessageResourceFromEntityAssembler;
-import com.acme.fromzeroapi.iam.interfaces.acl.IamContextFacade;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,24 +22,27 @@ import java.util.stream.Collectors;
 public class MessageController {
 
     private final MessageCommandService messageCommandService;
-    private final IamContextFacade iamContextFacade;
+    private final ExternalIamMessageService externalIamMessageService;
     private final MessageQueryService messageQueryService;
 
-    public MessageController(MessageCommandService messageCommandService, IamContextFacade iamContextFacade, MessageQueryService messageQueryService) {
+    public MessageController(
+            MessageCommandService messageCommandService,
+            ExternalIamMessageService externalIamMessageService,
+            MessageQueryService messageQueryService) {
         this.messageCommandService = messageCommandService;
-        this.iamContextFacade = iamContextFacade;
+        this.externalIamMessageService = externalIamMessageService;
         this.messageQueryService = messageQueryService;
     }
 
     @PostMapping
     public ResponseEntity<MessageResource>createMessage(@RequestBody CreateMessageResource resource) {
 
-        var recipientUser = this.iamContextFacade.getUserById(resource.recipientId());
+        var recipientUser = this.externalIamMessageService.getUserByIdExternalService(resource.recipientId());
         if (recipientUser == null) {
             return ResponseEntity.badRequest().build();
         }
 
-        var senderUser = this.iamContextFacade.getUserById(resource.senderId());
+        var senderUser = this.externalIamMessageService.getUserByIdExternalService(resource.senderId());
         if (senderUser == null) {
             return ResponseEntity.badRequest().build();
         }
@@ -56,7 +59,7 @@ public class MessageController {
 
     @GetMapping(value = "/recipient/{recipientId}")
     public ResponseEntity<List<MessageResource>> getMessages(@PathVariable Long recipientId) {
-        var user = this.iamContextFacade.getUserById(recipientId);
+        var user = this.externalIamMessageService.getUserByIdExternalService(recipientId);
         if (user == null) {
             return ResponseEntity.badRequest().build();
         }
